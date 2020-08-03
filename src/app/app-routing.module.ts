942d3d70845e19848f2b6ce4ebf74fd9cda613d2
Laribi Houssem <laribihoussem1998@gmail.com>
2020-08-03 16:21:24 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component'
+import {ProfileComponent} from './components/profile/profile.component'
+import {LoginComponent} from './components/login/login.component'
+import {RegisterComponent} from './components/register/register.component'
+import { PublicationsComponent } from './components/publications/publications.component'
+
+const appRoutes: Routes =[
+  { path:'', component: HomeComponent },
+  { path:'home', component: HomeComponent },
+  { path:'profile', component: ProfileComponent  },
+  { path:'login', component: LoginComponent },
+  { path:'register', component: RegisterComponent },
+  { path:'publications', component: PublicationsComponent }
 
 
-const routes: Routes = [];
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from "@angular/router"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { PublicationsComponent } from './components/publications/publications.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
+    PublicationsComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
